382d40829ea71b6853c78b1e728061b4af357b73
Anshika <akanshikakalra@gmail.com>
2025-10-29 12:03:45 +0530

    changes

--- srow_frontend/src/features/authSlice.ts
+++ srow_frontend/src/features/authSlice.ts
@@ -2,7 +2,8 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/auth";
+const API_URL = import.meta.env.VITE_BACKEND_URL + "/api/auth"
+
 
 interface User {
   _id: string;
