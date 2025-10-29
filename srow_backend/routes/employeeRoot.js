8662f954ef5f33f2c1be160a9d273965ea6dda5a
Anshika <akanshikakalra@gmail.com>
2025-10-29 11:44:50 +0530

    change in backend

--- srow_backend/routes/employeeRoot.js
+++ srow_backend/routes/employeeRoot.js
@@ -1,5 +1,5 @@
 import express from "express";
-import Route from "../models/route.js";   // ✅ Use capitalized model name
+import Route from "../models/Route.js";   // ✅ Use capitalized model name
 import { auth } from "../middleware/auth.js";
 
 const router = express.Router();
